21cea17d7763aa5643e9e479218d94b34973c28c
Ahmed Nadim <ahmed.mahmoud.nadim@gmail.com>
2025-06-14 09:18:10 +0300

    Use Geolocation

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+////////////////////////////////////////////////////////////////////////////////
+
+//========= Use Geolocation =========//
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+    },
+    function () {
+      alert`I'm Your Servant, 😥But I can't Get Your Location`;
+    }
+  );
+}
